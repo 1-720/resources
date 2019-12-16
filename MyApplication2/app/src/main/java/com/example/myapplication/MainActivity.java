217a1186d5b512c431b1579158a0c56b062f7006
HH <speculation1007@gmail.com>
2019-12-17 07:16:13 +0900

    first commit

---
+++ MyApplication2/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.myapplication;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottom_nav);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+        }
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                Fragment fragment = null;
+
+                switch (menuItem.getItemId()) {
+                    case R.id.home:
+                        fragment = new HomeFragment();
+                        break;
+                    case R.id.search:
+                        fragment = new SearchFragment();
+                        break;
+                    case R.id.bookmark:
+                        fragment = new BookmarkFragment();
+                        break;
+                }
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, fragment).commit();
+                return true;
+            }
+        });
+
+    }
+}
